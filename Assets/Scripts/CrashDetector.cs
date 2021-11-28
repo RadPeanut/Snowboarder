8186aab41680e2472a5ac9f0c3ccfa9f54aa2f09
Chris Armstrong <dev.radpeanut@gmail.com>
2021-11-28 17:28:15 -0600

    Added finish line and crash detector

---
+++ Assets/Scripts/CrashDetector.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrashDetector : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Ground") {
+            Debug.Log("Ouch");
+        }
+    }
+}
