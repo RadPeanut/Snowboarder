a1a9c94e9d99dc0ebf6870aa0e23ff11fc7c31cb
Chris Armstrong <dev.radpeanut@gmail.com>
2021-11-30 17:55:29 -0600

    Added particle effects and sound

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -1,13 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] float delayBeforeRestart = 2.0f;
+    [SerializeField] ParticleSystem crashEffect;
+    [SerializeField] AudioClip crashSFX;
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Ground") {
-            Debug.Log("Ouch");
+            crashEffect.Play();
+            GetComponent<AudioSource>().PlayOneShot(crashSFX);
+            Invoke("ReloadScene", delayBeforeRestart);
         }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -1,12 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] float delayBeforeRestart = 2.0f;
+    [SerializeField] ParticleSystem finishEffect;
+
     void OnTriggerEnter2D(Collider2D collision) {
         if (collision.tag == "Player") {
-            Debug.Log("You finished!");
+            finishEffect.Play();
+            GetComponent<AudioSource>().Play();
+            Invoke("ReloadScene", delayBeforeRestart);
         }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
